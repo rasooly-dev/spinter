6f99e9c9f356efce997e75ad1f544c03cab46147
Alex <alex@Alexs-MacBook-Pro.local>
2022-10-16 06:27:56 -0700

    Pulling most recent repo

--- server/generic_functions.py
+++ server/generic_functions.py
@@ -3,30 +3,32 @@ from collections import heapq
 
 #generic_filler_words = set('')
 
+
 def mp3_from_mp4(path_to_mp4, path_to_mp3):
-	video_clip = VideoFileClip(path_to_mp4)
-	audio_clip = video_clip.audio
-	audio_clip.write_audiofile(path_to_mp3)
-	audio_clip.close()
-	video_clip.close()
+    video_clip = VideoFileClip(path_to_mp4)
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile(path_to_mp3)
+    audio_clip.close()
+    video_clip.close()
+
 
 def mostCommonWords(text, n):
-	new_text = text.replace('.','')
-	text_arr = new_text.split()
+    new_text = text.replace('.', '')
+    text_arr = new_text.split()
 
-	wordCount = {}
-	for word in text_arr:
-		word = word.lower()
-		wordCount[word] = 1 + wordCount.get(word, 0)
+    wordCount = {}
+    for word in text_arr:
+        word = word.lower()
+        wordCount[word] = 1 + wordCount.get(word, 0)
 
-	maxHeap = [(-freq, word) for word, freq in wordCount.items()]
-	heapq.heapify(maxHeap)
+    maxHeap = [(-freq, word) for word, freq in wordCount.items()]
+    heapq.heapify(maxHeap)
 
-	mostCommon = {}
-	for _ in range(n):
-		word = heapq.heappop(maxHeap)
-		mostCommon[word[1]]: -word[0]
-	return mostCommon
+    mostCommon = {}
+    for _ in range(n):
+        word = heapq.heappop(maxHeap)
+        mostCommon[word[1]]: -word[0]
+    return mostCommon
 
 
 # Testing
