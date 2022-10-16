9b00e926a4f4ef3678d83bbff7c07a5d2c03b961
Alex <alex@Alexs-MacBook-Pro.local>
2022-10-16 03:48:44 -0700

    Pushing cohere_utils

--- server/cohere_utils.py
+++ server/cohere_utils.py
@@ -5,66 +5,83 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
+
 COHERE_API_KEY = os.getenv('COHERE_API_KEY')
-COHERE_API_TOKENS = os.getenv('COHERE_API_TOKENS')
-COHERE_API_TEMPERATURE = os.getenv('COHERE_API_TEMPERATURE')
-NUMBER_OF_QUESTIONS = os.getenv('NUMBER_OF_QUESTIONS')
+COHERE_API_QUES_TOKENS = int(os.getenv('COHERE_API_QUES_TOKENS'))
+COHERE_API_ANS_TOKENS = int(os.getenv('COHERE_API_ANS_TOKENS'))
+COHERE_API_TEMPERATURE = float(os.getenv('COHERE_API_TEMPERATURE'))
+NUMBER_OF_QUESTIONS = int(os.getenv('NUMBER_OF_QUESTIONS'))
+
+GEN_QUES_PROMPT = """Job Title: {}
+Technical Skills: {}
+Soft Skills: {}
+Job Description: {}
+Given the above information, generate one open-ended interview question that an interviewer would ask a candidate with the above technical skills for the job with the given description:
+Q1."""
+
+GEN_ANS_PROMPT = """Job Title: {}
+Technical Skills: {}
+Soft Skills: {}
+Job Description: {}
+Given the above information, the user was asked the following question:
+Q1. {}
+Original Response: {}
+
+
+Create a new improved response to the question using the technical skills:
+Improved response:"""
 
 
 co = cohere.Client(COHERE_API_KEY)  # initalize cohere client
 
 
-def generate(_prompt):
+def generate(_prompt, _max_tokens=COHERE_API_QUES_TOKENS):
     response = co.generate(prompt=_prompt,
-                           max_tokens=COHERE_API_TOKENS,
-                           temperature=COHERE_API_TEMPERATURE)
+                           max_tokens=_max_tokens,
+                           temperature=COHERE_API_TEMPERATURE,
+                           model='xlarge',
+                           stop_sequences=["\n"],
+                           frequency_penalty=0,
+                           presence_penalty=0,
+                           p=0.75,
+                           k=0)
     return response
 
 
-# returns JSON object with questions and answer pairs
-def generate_questions_and_answers(criteria):
-    base_prompt = f"Job Title: {criteria['job-title']}\n"\
-        f"Soft Skills: {criteria['qualifications']['soft-skills']}\n"\
-        f"Technical Skills: {criteria['qualifications']['technical-skills']}\n"\
-        f"Job Description: {criteria['job-description']}\n"
+def generate_question(criteria):
+    tech_str = ""
+    soft_str = ""
 
-    if criteria['interview-questions']:
-        questions = criteria['interview-questions']
-    else:
-        questions_prompt = base_prompt + f"Given the information above, generate {NUMBER_OF_QUESTIONS} "\
-            "interview questions separated by commas for this job position:\n"
-        # need to parse this output to match the one from the if statement
-        questions = generate(questions_prompt).generations[0].text.split(",")
-        questions = [question.strip() for question in questions]
+    for tech in criteria['qualifications']['technical-skills']:
+        tech_str += tech + ", "
+    for soft in criteria['qualifications']['soft-skills']:
+        soft_str += soft + ", "
 
-    answers = []
-    for question in questions:
-        answer_prompt = base_prompt + \
-            f"Given the information above, answer the following question: {question}\n Answer: "
-        answers.append(generate(answer_prompt).generations[0].text.strip())
+    prompt = GEN_QUES_PROMPT.format(
+        criteria['job-title'], tech_str, soft_str, criteria['job-description'])
 
-    question_answer_pairs = []
-    for i in range(len(questions)):
-        question_answer_pairs.append({'question': questions[i],
-                                      'preferred-answer': answers[i]})
-
-    data = {'data': question_answer_pairs}
-    return json.dumps(data)
-
-
-# returns improved answer as string
-def generate_improved_answer(criteria, response, qa_pair, common_words):
-    base_prompt = f"Job Title: {criteria['job-title']}\n"\
-        f"Soft Skills: {criteria['qualifications']['soft-skills']}\n"\
-        f"Technical Skills: {criteria['qualifications']['technical-skills']}\n"\
-        f"Job Description: {criteria['job-description']}\n"
-
-    prompt = base_prompt + "Given the above information, "\
-        f"the user was asked the following question: {qa_pair['question']} \n"\
-        f"Their original response was: {response} \n" \
-        f"A better response would have been: {qa_pair['preferred-answer']} \n" \
-        f"They also used the following common words too much: \n"
-    for word in common_words.keys():
-        prompt += f"{word} was used {common_words[word]} times.\n"
-    prompt += "Using the better response, improve the original response without too many common words: \n"
     return generate(prompt).generations[0].text.strip()
+
+
+def generate_questions(criteria, num_questions=NUMBER_OF_QUESTIONS):
+    questions = []
+    for i in range(num_questions):
+        question = generate_question(criteria)
+        # questions.append(question.strip()[0:question.strip().rfind('?')+1])
+        questions.append(question)
+
+    return questions
+
+
+def generate_alternative(criteria, response, question):
+    tech_str = ""
+    soft_str = ""
+
+    for tech in criteria['qualifications']['technical-skills']:
+        tech_str += tech + ", "
+    for soft in criteria['qualifications']['soft-skills']:
+        soft_str += soft + ", "
+
+    prompt = GEN_ANS_PROMPT.format(
+        criteria['job-title'], tech_str, soft_str, criteria['job-description'], question, response)
+    return generate(prompt, COHERE_API_ANS_TOKENS).generations[0].text.strip()
